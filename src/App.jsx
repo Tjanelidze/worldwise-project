ee8037f5e6af119f9e16c553a81a36ca1dc1fb24
Tedo <tedojanelidze7@gmail.com>
2023-12-03 00:31:59 +0400

    added components implemented city list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -5,16 +6,48 @@ import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList';
+import { useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:8000';
 
 export default function App() {
+  const [cities, setCitites] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsloading(true);
+        const rest = await fetch(`${BASE_URL}/cities`);
+        const data = await rest.json();
+        setCitites(data);
+      } catch (error) {
+        alert('There was an error loading data...');
+      } finally {
+        setIsloading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
