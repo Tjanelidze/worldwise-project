42d17353f930dd0fe4f56d6d4fb92e5a34eef8d6
Tedo <tedojanelidze7@gmail.com>
2023-12-04 12:09:33 +0400

    modiefied country list so i can sort it with distinguished countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
@@ -7,7 +8,7 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
-import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -44,8 +45,14 @@ export default function App() {
             index
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="cities" element={<CityList />} />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import Spinner from './Spinner';
+import styles from './CountryList.module.css';
+import CountryItem from './CountryItem';
+import Message from './Message';
+
+export default function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country, i) => (
+        <CountryItem country={country} key={i} />
+      ))}
+    </ul>
+  );
+}
