7e726c027642b83d6932d25edd34675103225843
Tedo <tedojanelidze7@gmail.com>
2023-12-08 15:38:52 +0400

    implemented useReducer into CititesContext

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -49,7 +49,7 @@ export default function Map() {
           {isLoadingPosition ? 'Loading...' : 'Use your position'}
         </Button>
       )}
-      <MapContainer
+      {/* <MapContainer
         center={mapPosition}
         zoom={6}
         scrollWheelZoom={true}
@@ -58,6 +58,32 @@ export default function Map() {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>
+                {city.emoji} <span>{city.cityName}</span>
+              </span>
+            </Popup>
+          </Marker>
+        ))}
+
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer> */}
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {cities.map((city) => (
           <Marker
